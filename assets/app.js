9205ac84489f6a10f6d7fa9b624cc726271af894
Florian-Rowehy <florian.rowehy@gmail.com>
2020-11-23 10:50:39 +0100

    solved issue jquery not working

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,7 @@
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 //import $ from 'jquery';
 const $ = require('jquery');
-//require('bootstrap');
+require('bootstrap');
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
