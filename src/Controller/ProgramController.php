1e74373212cd03dfff139d3a33eb052046945544
Florian-Rowehy <florian.rowehy@gmail.com>
2020-11-26 09:50:05 +0100

    created findByCategory method

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,8 +18,19 @@ class ProgramController extends AbstractController
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
+            'programs' => $programs,
         ]);
     }
 
@@ -34,8 +47,54 @@ class ProgramController extends AbstractController
      */
     public function show($id): Response
     {
+        if (!$id) {
+            throw $this
+                ->createNotFoundException('No id has been sent to find a program in program\'s table.');
+        }
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->find($id);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$id.' title, found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
             'id' => $id,
+            'program' => $program,
+        ]);
+    }
+
+    /**
+     * @route(
+     *     "/category/{category}",
+     *     name="show_category",
+     *     requirements={"category"="^[a-z]+$"},
+     *     methods={"GET"}
+     *     )
+     * @param $category
+     * @return Response
+     */
+    public function showByCategory($category)
+    {
+        if (!$category) {
+            throw $this
+                ->createNotFoundException('No category argument');
+        }
+
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findByCategory($category);
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program with '.$category.' category found \'program\' table.'
+            );
+        }
+        return $this->render('program/category.html.twig', [
+            'programs' => $programs,
+            'category' => $category,
         ]);
     }
 }
