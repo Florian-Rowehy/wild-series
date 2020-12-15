1d96ba86bdb9e680991804bdc77949d64d54cf90
Florian-Rowehy <florian.rowehy@gmail.com>
2020-12-15 07:29:40 +0100

    when creating an episode, sends a email

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -8,6 +8,8 @@ use App\Repository\EpisodeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -28,7 +30,7 @@ class EpisodeController extends AbstractController
     /**
      * @Route("/new", name="episode_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, MailerInterface $mailer): Response
     {
         $episode = new Episode();
         $form = $this->createForm(EpisodeType::class, $episode);
@@ -38,6 +40,14 @@ class EpisodeController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($episode);
             $entityManager->flush();
+
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('me@gmail.com')
+                ->subject('A new episode is out!')
+                ->html($this->renderView('Episode/newEpisodeEmail.html.twig', ['episode' => $episode]));
+            ;
+            $mailer->send($email);
 
             return $this->redirectToRoute('episode_index');
         }
