71b9e3707a73b6416d97cf9f79c1dcc32d8ca3d3
Florian-Rowehy <florian.rowehy@gmail.com>
2021-01-05 12:20:26 +0100

    updated searchbar js & scss

--- assets/js/header-autocomplete.js
+++ assets/js/header-autocomplete.js
@@ -3,17 +3,28 @@ const listFoundFilms = document.getElementById('listFoundFilms');
 
 searchBar.addEventListener('input', (event)=>{
     const input = event.target.value;
+    if (!input) {
+        listFoundFilms.innerHTML = '';
+        listFoundFilms.classList.add('d-none')
+        return;
+    }
     const url = '/programs/search/'+ input;
     fetch(url)
         .then(response => response.json())
         .then(programs => {
-            listFoundFilms.replaceChildren(
-                ...programs.map( program => {
-                    const li = document.createElement('li');
-                    li.textContent = program.title;
-                    return li;
-                })
-            );
+            if (programs.length !== 0) {
+                listFoundFilms.classList.remove('d-none');
+                listFoundFilms.replaceChildren(
+                    ...programs.map(program => {
+                        const li = document.createElement('li');
+                        li.textContent = program.title;
+                        return li;
+                    })
+                );
+            } else {
+                listFoundFilms.innerHTML = '';
+                listFoundFilms.classList.add('d-none');
+            }
         })
         .catch(err => {
             console.log(err);
